Hotfix: normalization for DCGAN + CUDA fix

utils.py
@@ -81,7 +81,14 @@ class Hyperparameters():
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if torch.cuda.is_available():
+            nn.init.normal_(m.weight.data, 0.0, 0.02).cuda()
+        else:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        if torch.cuda.is_available():
+            nn.init.normal_(m.weight.data, 1.0, 0.02).cuda()
+            nn.init.constant_(m.bias.data, 0).cuda()
+        else:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)